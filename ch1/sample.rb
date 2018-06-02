abf79c26c272653d8137a713acad84b72634966d
yinm <yinm.cminor9@gmail.com>
2018-06-02 14:03:09 +0900

    Check module

--- ch1/sample.rb
+++ ch1/sample.rb
@@ -1,20 +1,11 @@
-class SuperMyClass
-  def super_say
-    puts 'super_hello'
+module MyModule
+  def say
+    puts 'hello'
+  end
+
+  def self.say
+    puts 'self_hello'
   end
 end
 
-class MyClass < SuperMyClass
-  def sub_say
-    puts 'sub_hello'
-  end
-
-  def super_say
-    puts 'sub_super_hello'
-    super
-  end
-end
-
-my_instance = MyClass.new
-my_instance.sub_say
-my_instance.super_say
+MyModule.say
