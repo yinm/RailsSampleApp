5f2207ebe391212bc7cd54baf828ce55485f64c2
yinm <yinm.cminor9@gmail.com>
2018-06-02 13:54:26 +0900

    Check rokect syntax

--- ch1/sample.rb
+++ ch1/sample.rb
@@ -1,15 +1,2 @@
-def method_with_proc
-  proc = Proc.new { return p "proc" }
-  proc.call
-  puts "method_proc"
-end
-
-method_with_proc
-
-def method_with_lambda
-  lambda1 = lambda { return p "lambda" }
-  lambda1.call
-  puts "method_lambda"
-end
-
-method_with_lambda
+twice = -> (x) { 2 * x }
+p twice.call(3)
