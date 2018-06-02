e33ea5fa752e1b1214fdbee10be004ab73651c59
yinm <yinm.cminor9@gmail.com>
2018-06-02 14:01:36 +0900

    Check override

--- ch1/sample.rb
+++ ch1/sample.rb
@@ -1,11 +1,20 @@
 class SuperMyClass
-  def initialize
-    @super_variable = 'init'
+  def super_say
+    puts 'super_hello'
   end
 end
 
 class MyClass < SuperMyClass
+  def sub_say
+    puts 'sub_hello'
+  end
+
+  def super_say
+    puts 'sub_super_hello'
+    super
+  end
 end
 
-p SuperMyClass.new
-p MyClass.new
+my_instance = MyClass.new
+my_instance.sub_say
+my_instance.super_say
