745edd7b6b4738ec2edeac621e219253ffa32fdf
yinm <yinm.cminor9@gmail.com>
2018-06-03 11:48:45 +0900

    remove scaffold

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -19,11 +19,4 @@ ActiveRecord::Schema.define(version: 2018_06_03_004430) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
